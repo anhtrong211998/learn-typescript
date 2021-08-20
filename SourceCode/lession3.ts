248fba2b313a4146b7ed5581c9ba48cf6939404b
anhtrong211998 <anhtrong211998@gmail.com>
2021-08-20 10:56:25 +0700

    Hoisting in typescript example

---
+++ SourceCode/lession3.ts
@@ -0,0 +1,9 @@
+function greetPerson(name: any){
+    if(name === 'Chandler'){
+        var greet = 'Hello Chandler';
+    }else{
+        var greet = 'Hi there';
+    }
+    console.log(greet);
+}
+greetPerson("Chandler");
